f8a55c7595c65e724e6e5080fe62c7152514ae66
Bryanth87 <tgbryan14@gmail.com>
2024-09-03 16:51:23 -0600

    build:proyecto inicializado

---
+++ src/main/java/com/losbraulios/hotelmate/HotelmateApplication.java
@@ -0,0 +1,13 @@
+package com.losbraulios.hotelmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelmateApplication.class, args);
+	}
+
+}
