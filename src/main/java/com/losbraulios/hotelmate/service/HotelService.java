e62a14be6869414787e8508f94c04fa398c27517
AlvarezBryan23 <balvarez-2023244@kinal.edu.gt>
2024-09-08 17:14:49 -0600

    add(Proyecto Final):Se creo y agrego la clase servicio

--- src/main/java/com/losbraulios/hotelmate/service/HotelService.java
+++ src/main/java/com/losbraulios/hotelmate/service/HotelService.java
@@ -1,5 +1,32 @@
 package com.losbraulios.hotelmate.service;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.losbraulios.hotelmate.models.Hotel;
+import com.losbraulios.hotelmate.repository.HotelRepository;
 
-public class HotelService {
+@Service
+public class HotelService implements IHotelService{
+    @Autowired
+    HotelRepository hotelRepository;
 
+    @Override
+    public List<Hotel> listHotel(){
+        return hotelRepository.findAll();
+    }
+
+    @Override
+    public Hotel getHotel(Long idHotel){
+        return hotelRepository.findById(idHotel).orElse(null);
+    }
+
+    @Override
+    public Hotel register(Hotel hotel){
+        return hotelRepository.save(hotel);
+    }
+
+    @Override
+    public void eliminate(Hotel hotel) {
+        hotelRepository.delete(hotel);
+    }
 }
