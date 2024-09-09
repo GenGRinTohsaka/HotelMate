8b732ca529e86a579d387ebbbf256e8a0cd27e40
Johan Miguel <jtojin-2020591@kinal.edu.gt>
2024-09-08 19:16:17 -0600

    feat(Proyecto Final): se complemento la Iterface de Habitaciones

---
+++ src/main/java/com/losbraulios/hotelmate/service/IHabitacionService.java
@@ -0,0 +1,16 @@
+package com.losbraulios.hotelmate.service;
+
+import java.util.List;
+
+import com.losbraulios.hotelmate.models.Habitaciones;
+
+public interface IHabitacionService {
+    List<Habitaciones> listHabitaciones();
+
+
+    Habitaciones gHabitaciones(Long idHabitacion);
+    Habitaciones register(Habitaciones habitacion);
+    void eliminate(Habitaciones habitacion);
+
+}
+
