3131a8fe0f3e3100bc9e2405b002304f064c70c4
RinTohsaka <tohsakaringod@gmail.com>
2024-09-08 22:47:45 -0600

    feat:se configuro la clase RoomsAssignmentDTO

--- src/main/java/com/losbraulios/hotelmate/DTO/RoomsAssignmentDTO.java
+++ src/main/java/com/losbraulios/hotelmate/DTO/RoomsAssignmentDTO.java
@@ -1,5 +1,19 @@
 package com.losbraulios.hotelmate.DTO;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class RoomsAssignmentDTO {
 
+    @NotBlank(message = "El numero de habitacion no puede estar vacio")
+    private long numeroHabitacion;
+    @NotBlank(message = "El precio no puede ir vacio")
+    private double precioNoche;
+    @NotBlank(message = "El precio no puede ir vacio")
+    private double precioDia;
+    @NotBlank(message = "El tipo de la habitacion no puede ir vacia")
+    private String tipoHabitacion;
+    @NotBlank(message = "La capacidad no puede ir vacia")
+    private String capacidadHabitacion;
 }
