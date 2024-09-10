24c1a06d3c64c2e66586ba6ddeacd052f7f5fa88
Fredy Hernández <fhernandez-2023176@kinal.edu.gt>
2024-09-09 21:39:03 -0600

    add: Se añadia la clase Employees en la carpeta models

--- src/main/java/com/losbraulios/hotelmate/models/Employees.java
+++ src/main/java/com/losbraulios/hotelmate/models/Employees.java
@@ -1,5 +1,33 @@
 package com.losbraulios.hotelmate.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employees {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long employeeId;
+    @NotBlank
+    private String employeeName;
+    @NotBlank
+    private String employeeLastName;
+    @NotBlank
+    private String employeePhone;
+    @NotBlank
+    @Column(unique = true)
+    private String employeeEmail;
+    @NotBlank
+    private String employeeRole;
 
 }
