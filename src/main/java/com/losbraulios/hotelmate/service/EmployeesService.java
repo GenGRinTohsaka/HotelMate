3679c043b2ced703b8f02cff8226310395ce3c76
AlvarezBryan23 <balvarez-2023244@kinal.edu.gt>
2024-09-10 21:51:56 -0600

    build(Proyecto Final):Se creo y agrego la clase de servicio para empleados con su crud completo

--- src/main/java/com/losbraulios/hotelmate/service/EmployeesService.java
+++ src/main/java/com/losbraulios/hotelmate/service/EmployeesService.java
@@ -1,5 +1,33 @@
 package com.losbraulios.hotelmate.service;
 
-public class EmployeesService {
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.losbraulios.hotelmate.models.Employees;
+import com.losbraulios.hotelmate.repository.EmployeesRepository;
 
+@Service
+public class EmployeesService implements IEmployeesService{
+    @Autowired
+    EmployeesRepository employeesRepository;
+
+    @Override
+    public List<Employees> listEmployees() {
+        return employeesRepository.findAll();
+    }
+
+    @Override
+    public Employees getEmployees(Long idEmployee) {
+        return employeesRepository.findById(idEmployee).orElse(null);
+    }
+
+    @Override
+    public Employees register(Employees employees) {
+        return employeesRepository.save(employees);
+    }
+
+    @Override
+    public void eliminate(Employees employees) {
+        employeesRepository.delete(employees);
+    }
 }
