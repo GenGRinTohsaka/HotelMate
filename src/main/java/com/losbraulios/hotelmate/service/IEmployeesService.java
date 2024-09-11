d683fb80e981146f902da76b5bb2cd41b4ae86c2
AlvarezBryan23 <balvarez-2023244@kinal.edu.gt>
2024-09-10 21:14:25 -0600

    build(Proyecto Final):Se creo la interface del modelo Empleados con su crud completo

--- src/main/java/com/losbraulios/hotelmate/service/IEmployeesService.java
+++ src/main/java/com/losbraulios/hotelmate/service/IEmployeesService.java
@@ -1,5 +1,14 @@
 package com.losbraulios.hotelmate.service;
 
-public interface IEmployeesService {
+import java.util.List;
+import com.losbraulios.hotelmate.models.Employees;
 
+public interface IEmployeesService {
+    List<Employees> listEmployees();
+
+    Employees getEmployees(Long idEmployee);
+
+    Employees register(Employees employees);
+
+    void eliminate(Employees employees);
 }
