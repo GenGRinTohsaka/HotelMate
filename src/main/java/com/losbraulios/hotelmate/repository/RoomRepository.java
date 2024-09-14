988942104c8900dc2c820ab258a32cd95e050164
Johan Miguel <jtojin-2020591@kinal.edu.gt>
2024-09-14 12:14:37 -0600

    feat(Proyecto Final): Metodo personalizado busqueda de habitacion

--- src/main/java/com/losbraulios/hotelmate/repository/RoomRepository.java
+++ src/main/java/com/losbraulios/hotelmate/repository/RoomRepository.java
@@ -1,9 +1,13 @@
 package com.losbraulios.hotelmate.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.losbraulios.hotelmate.models.Hotel;
 import com.losbraulios.hotelmate.models.Rooms;
 
 public interface  RoomRepository extends JpaRepository <Rooms, Long>{
-    
+    //Metodo personalizado que busquedaa y devuelva todas las habitaciones de un hotel
+    List<Rooms> findByUser(Hotel hotel);
 }
