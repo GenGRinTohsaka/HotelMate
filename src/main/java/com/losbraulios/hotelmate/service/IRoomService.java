d8cc494ccad50285f571bc45e04f80b5e3b02ceb
Johan Miguel <jtojin-2020591@kinal.edu.gt>
2024-09-14 12:31:38 -0600

    feat(Proyecto Final): se completo la Interface de Room

--- src/main/java/com/losbraulios/hotelmate/service/IRoomService.java
+++ src/main/java/com/losbraulios/hotelmate/service/IRoomService.java
@@ -2,15 +2,18 @@ package com.losbraulios.hotelmate.service;
 
 import java.util.List;
 
+import com.losbraulios.hotelmate.DTO.RoomsAssignmentDTO;
+import com.losbraulios.hotelmate.DTO.RoomsResponseDTO;
 import com.losbraulios.hotelmate.models.Rooms;
 
 public interface IRoomService {
-    List<Rooms> listRooms();
+  //Método para listar habitaciones 
+  List<RoomsResponseDTO> myRooms(Long hotelId);
 
-    Rooms getRooms(Long idRoom); 
-    Rooms register(Rooms room);
-    void eliminate(Rooms room);
+  //Método para mostrar solo 1 habitacion por su Id
+  Rooms findByIdRoom(Long id);
 
-    
+  //Método para guardar una habitacion
+  Rooms save(RoomsAssignmentDTO roomDTO);
 
 }
