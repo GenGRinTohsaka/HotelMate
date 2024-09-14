7df1244ebed4fe3e92aafbae4843b8ebc92a051e
AlvarezBryan23 <balvarez-2023244@kinal.edu.gt>
2024-09-14 15:10:55 -0600

    add(Proyecto Final):Se creo y agrego las dos clases de DTO para los dos servicios

---
+++ src/main/java/com/losbraulios/hotelmate/DTO/EventsSaveDTO.java
@@ -0,0 +1,29 @@
+package com.losbraulios.hotelmate.DTO;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class EventsSaveDTO {
+    @NotNull(message =  "El nombre del evento no puede ir vacío")
+    private String eventName;
+    @NotNull(message = "La descripción del evento no puede ir vacío")
+    private String eventDescription;
+    @NotNull(message = "La hora de inicio no puede ir vacía")
+    @FutureOrPresent
+    private LocalTime startHour;
+    @NotNull(message = "La hora final no puede ir vacía")
+    @FutureOrPresent
+    private LocalTime endHour;
+    @NotNull(message = "El día de inicio no pude ir vacio")
+    @FutureOrPresent
+    private LocalDate startDate;
+    @NotNull(message = "El día final no puede ir vacío")
+    @FutureOrPresent
+    private LocalDate endDate;
+    @NotNull(message = "No selecciono un servicio")
+    private Long serviceId;
+}
