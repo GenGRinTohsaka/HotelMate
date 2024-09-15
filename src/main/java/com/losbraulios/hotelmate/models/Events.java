811e27280ede1d1d1dd64c41cde59e60c6a617a9
Johan Miguel <jtojin-2020591@kinal.edu.gt>
2024-09-14 20:26:24 -0600

    add(Proyecto Final):se agregan Modelos para hacer Interface y Service de Servicios

---
+++ src/main/java/com/losbraulios/hotelmate/models/Events.java
@@ -0,0 +1,45 @@
+package com.losbraulios.hotelmate.models;
+
+import java.security.Timestamp;
+import java.time.LocalTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Events {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long eventId;
+    @NotBlank
+    private String eventName;
+    @NotBlank
+    private String eventDescription;
+    @NotNull
+    @FutureOrPresent
+    private LocalTime startHour;
+    @NotNull
+    @FutureOrPresent
+    private LocalTime endHour;
+    @NotNull
+    @FutureOrPresent
+    private Timestamp startDate;
+    @NotNull
+    @FutureOrPresent
+    private Timestamp endDate;
+    @NotNull
+    @OneToOne
+    private Services services;
+}
