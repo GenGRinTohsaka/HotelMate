92f699f1c722dde3305fe806948ba483f01501b3
Johan Miguel <jtojin-2020591@kinal.edu.gt>
2024-09-15 09:39:29 -0600

    feat(Proyecto Final): se agregaron Anotaciones para El funcionamiento

--- src/main/java/com/losbraulios/hotelmate/DTO/ServiceSaveDTO.java
+++ src/main/java/com/losbraulios/hotelmate/DTO/ServiceSaveDTO.java
@@ -1,7 +1,15 @@
 package com.losbraulios.hotelmate.DTO;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ServiceSaveDTO {
     private Long serviceId;
     @NotNull(message = "El nombre del servicio no puede estar vacio")
