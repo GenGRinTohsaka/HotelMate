5892d31508bfdf3a7a99a88e19c72b528fb7c92b
Johan Miguel <jtojin-2020591@kinal.edu.gt>
2024-09-15 09:36:12 -0600

    feat(Proyecto Final): se Modifica ServiceRepository Pra Events

--- src/main/java/com/losbraulios/hotelmate/repository/ServicesRepository.java
+++ src/main/java/com/losbraulios/hotelmate/repository/ServicesRepository.java
@@ -7,6 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.losbraulios.hotelmate.models.Services;
 
 public interface ServicesRepository extends JpaRepository<Services, Long>{
-    //Metodo para devolver Servicios de Un evento
-    List<Services> findByServices(Services services);
+    // Se implementara en Events 
 }
