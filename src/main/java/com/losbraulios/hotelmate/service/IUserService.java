c7de91100c9fb9273a6cde3521be67a8a8c55ad2
Johan Miguel <jtojin-2020591@kinal.edu.gt>
2024-09-16 14:21:41 -0600

    add(Proyecto Final):Se avanza con Service e Interface para User

--- src/main/java/com/losbraulios/hotelmate/service/IUserService.java
+++ src/main/java/com/losbraulios/hotelmate/service/IUserService.java
@@ -1,5 +1,15 @@
 package com.losbraulios.hotelmate.service;
 
+import java.util.List;
+
+import com.losbraulios.hotelmate.models.Users;
+
 public interface  IUserService {
-    
+    List<Users> listUsers();
+
+    Users getUser(Long idUser);
+
+    Users register(Users user);
+
+    void eliminate(Users user);
 }
--- src/main/java/com/losbraulios/hotelmate/service/UserService.java
+++ src/main/java/com/losbraulios/hotelmate/service/UserService.java
@@ -1,5 +1,35 @@
 package com.losbraulios.hotelmate.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.losbraulios.hotelmate.models.Users;
+import com.losbraulios.hotelmate.repository.UserRepository;
+
+@Service
 public class UserService implements IUserService{
-    
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public List<Users> listUsers(){
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Users getUser(Long idUsers){
+        return userRepository.findById(idUsers).orElse(null);
+    }
+
+    @Override
+    public Users register(Users user){
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void eliminate(Users user) {
+        userRepository.delete(user);
+    }
 }
