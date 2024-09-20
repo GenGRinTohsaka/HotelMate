f806e9823c48e04f6101446d6a1db86de9f799ed
AlvarezBryan23 <balvarez-2023244@kinal.edu.gt>
2024-09-19 20:03:25 -0600

    add(Proyecto Final): Se creo la clase Clientes para su servicio y repositorio

--- src/main/java/com/losbraulios/hotelmate/repository/ServicesRepository.java
+++ src/main/java/com/losbraulios/hotelmate/repository/ServicesRepository.java
@@ -1,8 +1,11 @@
 package com.losbraulios.hotelmate.repository;
 
+
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.losbraulios.hotelmate.models.Services;
 
 public interface ServicesRepository extends JpaRepository<Services, Long>{
     
+     List<Services> findByServices(Services services);
 }
