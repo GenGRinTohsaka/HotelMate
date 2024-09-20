31f48cbadc6b1e07ec5d1f2558a9d9f8eb1743e1
AlvarezBryan23 <balvarez-2023244@kinal.edu.gt>
2024-09-20 12:22:29 -0600

    add(Proyecto Final): Se creo la clase reservacion para trabajar

---
+++ src/main/java/com/losbraulios/hotelmate/models/Reservations.java
@@ -0,0 +1,39 @@
+package com.losbraulios.hotelmate.models;
+
+import java.security.Timestamp;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Reservations {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idReservation;
+    @NotBlank
+    private String descriptionReservation;
+    @NotNull
+    @FutureOrPresent
+    private Timestamp starDate;
+    @NotNull
+    @FutureOrPresent
+    private Timestamp endDate;
+    @NotNull
+    @ManyToMany
+    private Rooms room;
+    @NotNull
+    @OneToMany
+    private Clients clients;
+}
