8a008e9c9b716e42df26dbfd0d77c3d73fcfaf51
Johan Miguel <jtojin-2020591@kinal.edu.gt>
2024-09-19 22:45:54 -0600

    add(Proyecto Final):se agrego Model de Factura

---
+++ src/main/java/com/losbraulios/hotelmate/models/Bills.java
@@ -0,0 +1,29 @@
+package com.losbraulios.hotelmate.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Bills {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idBill;
+    @NotNull
+    private Double totalBill;
+    @NotBlank
+    private String paymentMethod;
+    @NotNull
+    @OneToMany
+    private Clients clients;
+}
