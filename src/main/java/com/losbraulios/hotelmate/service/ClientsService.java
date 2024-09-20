6716979a00cf316d700e73b6390bacf091adc845
Johan Miguel <jtojin-2020591@kinal.edu.gt>
2024-09-20 00:19:46 -0600

    add(Proyecto Final):se agregaron archivos de Clientes

---
+++ src/main/java/com/losbraulios/hotelmate/service/ClientsService.java
@@ -0,0 +1,33 @@
+package com.losbraulios.hotelmate.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.losbraulios.hotelmate.models.Clients;
+import com.losbraulios.hotelmate.repository.ClientsRepository;
+
+@Service
+public class ClientsService implements IClientsService{
+    @Autowired
+    ClientsRepository clientsRepository;
+
+    @Override
+    public List<Clients> listClients() {
+        return clientsRepository.findAll();
+    }
+
+    @Override
+    public Clients getClients(Long idClient) {
+        return clientsRepository.findById(idClient).orElse(null);
+    }
+
+    @Override
+    public Clients register(Clients clients) {
+        return clientsRepository.save(clients);
+    }
+
+    @Override
+    public void eliminate(Clients clients) {
+        clientsRepository.delete(clients);
+    }
+}
---
+++ src/main/java/com/losbraulios/hotelmate/service/IClientsService.java
@@ -0,0 +1,15 @@
+package com.losbraulios.hotelmate.service;
+
+import java.util.List;
+
+import com.losbraulios.hotelmate.models.Clients;
+
+public interface IClientsService {
+    List<Clients> listClients();
+
+    Clients getClients(Long idClient);
+
+    Clients register(Clients clients);
+
+    void eliminate(Clients clients);
+}
