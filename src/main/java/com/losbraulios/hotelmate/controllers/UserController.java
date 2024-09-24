9185234ab8ddd90ad47f4badcbb5fc6fe37770c7
RinTohsaka <tohsakaringod@gmail.com>
2024-09-24 09:28:33 -0600

    feat:se agrego la funcion saveUser

--- src/main/java/com/losbraulios/hotelmate/controllers/UserController.java
+++ src/main/java/com/losbraulios/hotelmate/controllers/UserController.java
@@ -1,16 +1,26 @@
 package com.losbraulios.hotelmate.controllers;
 
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.CannotCreateTransactionException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 
+import com.losbraulios.hotelmate.DTO.UserSaveDTO;
+import com.losbraulios.hotelmate.models.Users;
 import com.losbraulios.hotelmate.service.UserService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("hotelMate/v1/users")
@@ -35,6 +45,36 @@ public class UserController {
         } catch (Exception err) {
             res.put("message", "Error general al obtener los datos");
             res.put("Error", err.getMessage());
+            return ResponseEntity.internalServerError().body(res);
+        }
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<?> saveUser(
+            @Valid @ModelAttribute UserSaveDTO user,
+            BindingResult result) {
+        Map<String, Object> res = new HashMap<>();
+        if (result.hasErrors()) {
+            List<String> errors = result.getFieldErrors()
+                    .stream()
+                    .map(error -> error.getDefaultMessage())
+                    .collect(Collectors.toList());
+            res.put("message", "Error con las validaciones favor ingresar todos los campos");
+            res.put("Errores", errors);
+            return ResponseEntity.badRequest().body(res);
+        }try {
+            Users users = new Users(
+                user.getIdUser(),
+                user.getNameUser(),
+                user.getPasswordUser(),
+                user.getEmailUser()
+            );
+            userService.register(users);
+            res.put("message", "Usuario creado correctamente");
+            return ResponseEntity.ok(res);
+        } catch (Exception err) {
+            res.put("message", "Error al guardar el usuario, intente de nuevo");
+            res.put("error", err.getMessage());
             return ResponseEntity.internalServerError().body(res);
         }
     }
